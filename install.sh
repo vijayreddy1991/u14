4511bfc5c9a0c8774a4908f6419b6dafef52c0c9
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2020-05-29 16:54:16 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,143 +62,143 @@ apt-get update
 apt-get install  -y git="$GIT_VERSION"
 
 
-# Git-LFS throws a warning that can be ignored - https://github.com/git-lfs/git-lfs/issues/2837
-export GIT_LFS=2.6.1
-echo "================= Installing Git LFS ==================="
-curl -sS https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install  git-lfs="$GIT_LFS"
+# # Git-LFS throws a warning that can be ignored - https://github.com/git-lfs/git-lfs/issues/2837
+# export GIT_LFS=2.6.1
+# echo "================= Installing Git LFS ==================="
+# curl -sS https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+# sudo apt-get install  git-lfs="$GIT_LFS"
 
-export JQ_VERSION=1.3*
-echo "================= Adding JQ 1.3.x ==================="
-apt-get install  -y jq="$JQ_VERSION"
+# export JQ_VERSION=1.3*
+# echo "================= Adding JQ 1.3.x ==================="
+# apt-get install  -y jq="$JQ_VERSION"
 
-echo "================= Installing Node  ==================="
-. /u14/node/install.sh
+# echo "================= Installing Node  ==================="
+# . /u14/node/install.sh
 
-# Java throws warnings that not resolved yet - https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=791531;msg=5
-echo "================= Installing Java  ==================="
-. /u14/java/install.sh
+# # Java throws warnings that not resolved yet - https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=791531;msg=5
+# echo "================= Installing Java  ==================="
+# . /u14/java/install.sh
 
-echo "================= Installing Ruby  ==================="
-. /u14/ruby/install.sh
+# echo "================= Installing Ruby  ==================="
+# . /u14/ruby/install.sh
 
-export GCLOUD_VERSION=233.0*
-echo "================= Adding gcloud "$GCLOUD_VERSION"============"
-CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
-curl -sS https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update && sudo apt-get install google-cloud-sdk="$GCLOUD_VERSION"
+# export GCLOUD_VERSION=233.0*
+# echo "================= Adding gcloud "$GCLOUD_VERSION"============"
+# CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+# echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+# curl -sS https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# sudo apt-get update && sudo apt-get install google-cloud-sdk="$GCLOUD_VERSION"
 
-export KUBECTL_VERSION=v1.13.3
-echo "================= Adding kubectl "$KUBECTL_VERSION" ==================="
-curl -sSLO https://storage.googleapis.com/kubernetes-release/release/"$KUBECTL_VERSION"/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+# export KUBECTL_VERSION=v1.13.3
+# echo "================= Adding kubectl "$KUBECTL_VERSION" ==================="
+# curl -sSLO https://storage.googleapis.com/kubernetes-release/release/"$KUBECTL_VERSION"/bin/linux/amd64/kubectl
+# chmod +x ./kubectl
+# sudo mv ./kubectl /usr/local/bin/kubectl
 
-KOPS_VERSION=1.11.0
-echo "Installing KOPS version: $KOPS_VERSION"
-curl -LO https://github.com/kubernetes/kops/releases/download/"$KOPS_VERSION"/kops-linux-amd64
-chmod +x kops-linux-amd64
-mv kops-linux-amd64 /usr/local/bin/kops
+# KOPS_VERSION=1.11.0
+# echo "Installing KOPS version: $KOPS_VERSION"
+# curl -LO https://github.com/kubernetes/kops/releases/download/"$KOPS_VERSION"/kops-linux-amd64
+# chmod +x kops-linux-amd64
+# mv kops-linux-amd64 /usr/local/bin/kops
 
-HELM_VERSION=v2.12.3
-echo "Installing helm version: $HELM_VERSION"
-wget https://storage.googleapis.com/kubernetes-helm/helm-"$HELM_VERSION"-linux-amd64.tar.gz
-tar -zxvf helm-"$HELM_VERSION"-linux-amd64.tar.gz
-mv linux-amd64/helm /usr/local/bin/helm
-rm -rf linux-amd64
+# HELM_VERSION=v2.12.3
+# echo "Installing helm version: $HELM_VERSION"
+# wget https://storage.googleapis.com/kubernetes-helm/helm-"$HELM_VERSION"-linux-amd64.tar.gz
+# tar -zxvf helm-"$HELM_VERSION"-linux-amd64.tar.gz
+# mv linux-amd64/helm /usr/local/bin/helm
+# rm -rf linux-amd64
 
-export APACHE_LIBCLOUD=2.4.0
-echo "================= Adding apache libcloud "$APACHE_LIBCLOUD" ============"
-sudo pip install apache-libcloud=="$APACHE_LIBCLOUD"
+# export APACHE_LIBCLOUD=2.4.0
+# echo "================= Adding apache libcloud "$APACHE_LIBCLOUD" ============"
+# sudo pip install apache-libcloud=="$APACHE_LIBCLOUD"
 
-export AWS_VERSION=1.16.102
-echo "================= Adding awscli "$AWS_VERSION" ============"
-sudo pip install  awscli=="$AWS_VERSION"
+# export AWS_VERSION=1.16.102
+# echo "================= Adding awscli "$AWS_VERSION" ============"
+# sudo pip install  awscli=="$AWS_VERSION"
 
-export AWSEBCLI_VERSION=3.14.11
-echo "================= Adding awscli "$AWSEBCLI_VERSION" ============"
-sudo pip install  awsebcli=="$AWSEBCLI_VERSION"
+# export AWSEBCLI_VERSION=3.14.11
+# echo "================= Adding awscli "$AWSEBCLI_VERSION" ============"
+# sudo pip install  awsebcli=="$AWSEBCLI_VERSION"
 
-export OPENSTACKCLIENT_VERSION=3.17.0
-echo "================= Adding openstack client $OPENSTACKCLIENT_VERSION ============"
-sudo pip install python-openstackclient=="$OPENSTACKCLIENT_VERSION" --ignore-installed urllib3
+# export OPENSTACKCLIENT_VERSION=3.17.0
+# echo "================= Adding openstack client $OPENSTACKCLIENT_VERSION ============"
+# sudo pip install python-openstackclient=="$OPENSTACKCLIENT_VERSION" --ignore-installed urllib3
 
-export SHADE_VERSION=1.30.0
-echo "==================adding shade $SHADE_VERSION================"
-sudo pip install shade=="$SHADE_VERSION"
-
-
-AZURE_CLI_VERSION=2.0*
-echo "================ Adding azure-cli $AZURE_CLI_VERSION  =============="
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
-  sudo tee /etc/apt/sources.list.d/azure-cli.list
-sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
-sudo apt-get install -q apt-transport-https=1.0.1*
-sudo apt-get update && sudo apt-get install -y -q azure-cli=$AZURE_CLI_VERSION
-
-export DOCTL_VERSION=1.13.0
-echo "================= Adding doctl $DOCTL_VERSION============"
-curl -OL https://github.com/digitalocean/doctl/releases/download/v"$DOCTL_VERSION"/doctl-"$DOCTL_VERSION"-linux-amd64.tar.gz
-tar xf doctl-"$DOCTL_VERSION"-linux-amd64.tar.gz
-sudo mv doctl /usr/local/bin
-rm doctl-"$DOCTL_VERSION"-linux-amd64.tar.gz
+# export SHADE_VERSION=1.30.0
+# echo "==================adding shade $SHADE_VERSION================"
+# sudo pip install shade=="$SHADE_VERSION"
 
 
-export JFROG_VERSION=1.24.1
-echo "================= Adding jfrog-cli "$JFROG_VERSION"==================="
-wget -nv https://api.bintray.com/content/jfrog/jfrog-cli-go/"$JFROG_VERSION"/jfrog-cli-linux-amd64/jfrog?bt_package=jfrog-cli-linux-amd64 -O jfrog
-sudo chmod +x jfrog
-sudo mv jfrog /usr/bin/jfrog
+# AZURE_CLI_VERSION=2.0*
+# echo "================ Adding azure-cli $AZURE_CLI_VERSION  =============="
+# echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
+#   sudo tee /etc/apt/sources.list.d/azure-cli.list
+# sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+# sudo apt-get install -q apt-transport-https=1.0.1*
+# sudo apt-get update && sudo apt-get install -y -q azure-cli=$AZURE_CLI_VERSION
 
-export ANSIBLE_VERSION=2.7.7
-echo "================ Adding ansible $ANSIBLE_VERSION===================="
-sudo pip install ansible=="$ANSIBLE_VERSION"
+# export DOCTL_VERSION=1.13.0
+# echo "================= Adding doctl $DOCTL_VERSION============"
+# curl -OL https://github.com/digitalocean/doctl/releases/download/v"$DOCTL_VERSION"/doctl-"$DOCTL_VERSION"-linux-amd64.tar.gz
+# tar xf doctl-"$DOCTL_VERSION"-linux-amd64.tar.gz
+# sudo mv doctl /usr/local/bin
+# rm doctl-"$DOCTL_VERSION"-linux-amd64.tar.gz
 
-export BOTO_VERSION=2.49.0
-echo "================ Adding boto $BOTO_VERSION ======================="
-sudo pip install  boto=="$BOTO_VERSION"
 
-export BOTO3_VERSION=1.9.92
-echo "============  Adding boto3 "$BOTO_VERSION" ==============="
-sudo pip install boto3=="$BOTO3_VERSION"
+# export JFROG_VERSION=1.24.1
+# echo "================= Adding jfrog-cli "$JFROG_VERSION"==================="
+# wget -nv https://api.bintray.com/content/jfrog/jfrog-cli-go/"$JFROG_VERSION"/jfrog-cli-linux-amd64/jfrog?bt_package=jfrog-cli-linux-amd64 -O jfrog
+# sudo chmod +x jfrog
+# sudo mv jfrog /usr/bin/jfrog
 
-export AZURE_VERSION=3.0
-echo "================ Adding azure $AZURE_VERSION ======================="
-sudo pip install azure=="$AZURE_VERSION"
+# export ANSIBLE_VERSION=2.7.7
+# echo "================ Adding ansible $ANSIBLE_VERSION===================="
+# sudo pip install ansible=="$ANSIBLE_VERSION"
 
-export DOPY_VERSION=0.3.7
-echo "================ Adding dopy $DOPY_VERSION ======================="
-sudo pip install dopy=="$DOPY_VERSION"
+# export BOTO_VERSION=2.49.0
+# echo "================ Adding boto $BOTO_VERSION ======================="
+# sudo pip install  boto=="$BOTO_VERSION"
 
-export TF_VERSION=0.11.11
-echo "================ Adding terraform-$TF_VERSION===================="
-export TF_FILE=terraform_"$TF_VERSION"_linux_amd64.zip
+# export BOTO3_VERSION=1.9.92
+# echo "============  Adding boto3 "$BOTO_VERSION" ==============="
+# sudo pip install boto3=="$BOTO3_VERSION"
 
-echo "Fetching terraform"
-echo "-----------------------------------"
-rm -rf /tmp/terraform
-mkdir -p /tmp/terraform
-wget -nv https://releases.hashicorp.com/terraform/$TF_VERSION/$TF_FILE
-unzip -o $TF_FILE -d /tmp/terraform
-sudo chmod +x /tmp/terraform/terraform
-mv /tmp/terraform/terraform /usr/bin/terraform
+# export AZURE_VERSION=3.0
+# echo "================ Adding azure $AZURE_VERSION ======================="
+# sudo pip install azure=="$AZURE_VERSION"
 
-echo "Added terraform successfully"
-echo "-----------------------------------"
+# export DOPY_VERSION=0.3.7
+# echo "================ Adding dopy $DOPY_VERSION ======================="
+# sudo pip install dopy=="$DOPY_VERSION"
 
-export PK_VERSION=1.3.4
-echo "================ Adding packer $PK_VERSION ===================="
-export PK_FILE=packer_"$PK_VERSION"_linux_amd64.zip
+# export TF_VERSION=0.11.11
+# echo "================ Adding terraform-$TF_VERSION===================="
+# export TF_FILE=terraform_"$TF_VERSION"_linux_amd64.zip
 
-echo "Fetching packer"
-echo "-----------------------------------"
-rm -rf /tmp/packer
-mkdir -p /tmp/packer
-wget -nv https://releases.hashicorp.com/packer/$PK_VERSION/$PK_FILE
-unzip -o $PK_FILE -d /tmp/packer
-sudo chmod +x /tmp/packer/packer
-mv /tmp/packer/packer /usr/bin/packer
+# echo "Fetching terraform"
+# echo "-----------------------------------"
+# rm -rf /tmp/terraform
+# mkdir -p /tmp/terraform
+# wget -nv https://releases.hashicorp.com/terraform/$TF_VERSION/$TF_FILE
+# unzip -o $TF_FILE -d /tmp/terraform
+# sudo chmod +x /tmp/terraform/terraform
+# mv /tmp/terraform/terraform /usr/bin/terraform
+
+# echo "Added terraform successfully"
+# echo "-----------------------------------"
+
+# export PK_VERSION=1.3.4
+# echo "================ Adding packer $PK_VERSION ===================="
+# export PK_FILE=packer_"$PK_VERSION"_linux_amd64.zip
+
+# echo "Fetching packer"
+# echo "-----------------------------------"
+# rm -rf /tmp/packer
+# mkdir -p /tmp/packer
+# wget -nv https://releases.hashicorp.com/packer/$PK_VERSION/$PK_FILE
+# unzip -o $PK_FILE -d /tmp/packer
+# sudo chmod +x /tmp/packer/packer
+# mv /tmp/packer/packer /usr/bin/packer
 
 echo "Added packer successfully"
 echo "-----------------------------------"
